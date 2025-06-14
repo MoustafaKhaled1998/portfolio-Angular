57883bd4aa763d035efb4458df1b90fb1aec0405
Moustafa <Mostax98@gmail.com>
2025-06-14 23:52:17 +0300

    Initial commit

---
+++ src/app/app.ts
@@ -0,0 +1,17 @@
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { Hero } from './hero/hero';
+import { About } from './about/about';
+import { Skills } from './skills/skills';
+import { Portfolio } from "./portfolio/portfolio";
+import { Footer } from "./footer/footer";
+
+@Component({
+  selector: 'app-root',
+  imports: [RouterOutlet, Hero, About, Skills, Portfolio, Footer],
+  templateUrl: './app.html',
+  styleUrl: './app.scss'
+})
+export class App {
+  protected title = 'project-1';
+}
